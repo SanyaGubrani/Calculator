a5a5f263355d18af850cd07b5078607524e8bf39
SanyaGubrani <127206660+SanyaGubrani@users.noreply.github.com>
2023-08-21 22:43:01 +0530

    initial code

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const display = document.querySelector('.display');
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+const equalsButton = document.querySelector('.equal');
+const equation = document.querySelectorAll('[data-value]'); 
+const operatorButtons = document.querySelectorAll('.operator');
+const buttons = document.querySelectorAll('.btn');
+
+
+
